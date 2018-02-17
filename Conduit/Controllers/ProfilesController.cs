e85595e357d620c3bd06cc86c8e29e04e798d5de
Cameron Ezra Jones <cejones90@msn.com>
2018-02-17 11:29:53 -0700

    Create follow and unfollow endpoints

--- Conduit/Controllers/ProfilesController.cs
+++ Conduit/Controllers/ProfilesController.cs
@@ -8,6 +8,7 @@ using Conduit.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Conduit.Controllers
 {
@@ -24,6 +25,7 @@ namespace Conduit.Controllers
             _userManager = userManager;
             _context = context;
         }
+
         // Get a profile
         [HttpGet("/api/profiles/{username}")]
         public async Task<IActionResult> GetProfile(string username)
@@ -61,6 +63,109 @@ namespace Conduit.Controllers
             }
             Profile profile = (Profile)requestedUser;
             profile.IsFollowing = isFollowing;
+            return Ok(profile);
+        }
+
+        //Follow a user
+        [HttpPost("/api/profiles/{username}/follow")]
+        public async Task<IActionResult> followUser(string username)
+        {
+            var authedUser = HttpContext.User;
+            if (!authedUser.HasClaim(c =>
+             c.Type == ClaimTypes.NameIdentifier))
+            {
+                this.HttpContext.Response.StatusCode = 401;
+                var authErrorResponse = new ErrorResponse();
+                authErrorResponse.addErrorKey("Missing authentication");
+                return Json(authErrorResponse);
+            }
+            string authUsername = authedUser.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier).Value;
+            ApplicationUser authedUserInDB = await _userManager.FindByNameAsync(authUsername);
+            if (null == authedUserInDB)
+            {
+                this.HttpContext.Response.StatusCode = 404;
+                var authErrorResponse = new ErrorResponse();
+                authErrorResponse.addErrorKey("Authenticated user not in database");
+                return Json(authErrorResponse);
+            }
+            ApplicationUser requestUser = await _userManager.FindByNameAsync(username);
+            if(null == requestUser)
+            {
+                this.HttpContext.Response.StatusCode = 404;
+                var authErrorResponse = new ErrorResponse();
+                authErrorResponse.addErrorKey("Requested user not in database");
+                return Json(authErrorResponse);
+            }
+            string authedUserId = authedUserInDB.Id;
+            string requestUserId = requestUser.Id;
+            try
+            {
+                UserIsFollowing userIsFollowing = new UserIsFollowing();
+                userIsFollowing.UserId = authedUserId;
+                userIsFollowing.IsFollowingId = requestUserId;
+                _context.UserIsFollowing.Add(userIsFollowing);
+                _context.SaveChanges();
+            } catch (DbUpdateException ex)
+            {
+                this.HttpContext.Response.StatusCode = 422;
+                var authErrorResponse = new ErrorResponse();
+                authErrorResponse.addErrorKey($"{authUsername} is already following {username}");
+                return Json(authErrorResponse);
+            }
+            Profile profile = (Profile)requestUser;
+            profile.IsFollowing = true;
+            return Ok(profile);
+        }
+
+        //Unollow a user
+        [HttpDelete("/api/profiles/{username}/follow")]
+        public async Task<IActionResult> unfollowUser(string username)
+        {
+            var authedUser = HttpContext.User;
+            if (!authedUser.HasClaim(c =>
+             c.Type == ClaimTypes.NameIdentifier))
+            {
+                this.HttpContext.Response.StatusCode = 401;
+                var authErrorResponse = new ErrorResponse();
+                authErrorResponse.addErrorKey("Missing authentication");
+                return Json(authErrorResponse);
+            }
+            string authUsername = authedUser.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier).Value;
+            ApplicationUser authedUserInDB = await _userManager.FindByNameAsync(authUsername);
+            if (null == authedUserInDB)
+            {
+                this.HttpContext.Response.StatusCode = 404;
+                var authErrorResponse = new ErrorResponse();
+                authErrorResponse.addErrorKey("Authenticated user not in database");
+                return Json(authErrorResponse);
+            }
+            ApplicationUser requestUser = await _userManager.FindByNameAsync(username);
+            if (null == requestUser)
+            {
+                this.HttpContext.Response.StatusCode = 404;
+                var authErrorResponse = new ErrorResponse();
+                authErrorResponse.addErrorKey("Requested user not in database");
+                return Json(authErrorResponse);
+            }
+            string authedUserId = authedUserInDB.Id;
+            string requestUserId = requestUser.Id;
+            try
+            {
+                UserIsFollowing userIsFollowing = new UserIsFollowing();
+                userIsFollowing.UserId = authedUserId;
+                userIsFollowing.IsFollowingId = requestUserId;
+                _context.UserIsFollowing.Remove(userIsFollowing);
+                _context.SaveChanges();
+            }
+            catch (DbUpdateException ex)
+            {
+                this.HttpContext.Response.StatusCode = 422;
+                var authErrorResponse = new ErrorResponse();
+                authErrorResponse.addErrorKey($"{authUsername} is already not following {username}");
+                return Json(authErrorResponse);
+            }
+            Profile profile = (Profile)requestUser;
+            profile.IsFollowing = false;
             return Ok(profile);
         }
     }
